b634a6b9aed2f02fa81b708ff4da64e7a377f1ab
Nguyen Tung Lam <lamnthnue.edu.vn@gmail.com>
2019-07-23 07:44:53 +0700

    'ok;

---
+++ src/KiemTraThang.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class KiemTraThang {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Which month that you want to count day");
+        int month = scanner.nextInt();
+
+        switch (month) {
+            case 2:
+                System.out.print("The month '2' has 28 or 29 day");
+                break;
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                System.out.print("The month '"+month+"' has 31 day");
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                System.out.print("The month '"+month+"' has 30 day");
+                break;
+        }
+    }
+}
